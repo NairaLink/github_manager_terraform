8d9ed457392a4543568c5f6fc568243e6f384378
Adebayo Samuel <eladebayoor@gmail.com>
2023-03-02 22:26:40 -0500

    [feat]: create teams and repos
    Signed-off-by: Adebayo Samuel <eladebayoor@gmail.com>

--- variables.tf
+++ variables.tf
@@ -10,6 +10,6 @@ variable "github_org" {
 
 variable "manage_repo" {
     type = string
-    default =""
+    default ="manage_github_terraform"
     description = "This is the name of your GitHub repository to manage the Organisation with Terraform"
 }
